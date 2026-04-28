2087f1a5f11f6ac6c192d4bcfa3b88e36a2cb946
jpmendeza2401 <jp.mendeza1@uniandes.edu.co>
2026-04-28 12:07:04 -0500

    create header component

---
+++ ParcialMISW4104/src/app/header/header.component.spec.ts
@@ -0,0 +1,31 @@
+/* tslint:disable:no-unused-variable */
+import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { HeaderComponent } from './header.component';
+
+describe('HeaderComponent', () => {
+  let component: HeaderComponent;
+  let fixture: ComponentFixture<HeaderComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [HeaderComponent],
+      providers: [
+        provideRouter([])
+      ]
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HeaderComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
