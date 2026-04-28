6fc5afdf91c16efc754be8cee325ef800c3301fb
jpmendeza2401 <jp.mendeza1@uniandes.edu.co>
2026-04-28 12:07:29 -0500

    create navbar component

---
+++ ParcialMISW4104/src/app/navbar/navbar.component.spec.ts
@@ -0,0 +1,31 @@
+/* tslint:disable:no-unused-variable */
+import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { NavbarComponent } from './navbar.component';
+
+describe('NavbarComponent', () => {
+  let component: NavbarComponent;
+  let fixture: ComponentFixture<NavbarComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [NavbarComponent],
+      providers: [
+        provideRouter([])
+      ]
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(NavbarComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
